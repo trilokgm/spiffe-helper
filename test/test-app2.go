49a778ee484fd884c619ce84a9b1a61b4b86214c
Trilok Geer <trilok.geer@gmail.com>
2018-10-09 15:24:03 +0530

    [Test code] added test apps for checking

---
+++ test/test-app2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"spiffe-helper/cmd/app"
+	"spiffe-helper/cmd/config"
+)
+func main() {
+
+	fmt.Printf(" Test app 2 - register workload api client \n")
+	config := config.SidecarConfig{
+		AgentAddress: "/tmp/agent.sock",
+		CertDir: "/opt/test-app2/certs/",
+		RenewSignal: "SIGUSR1",
+		SvidFileName: "svid.pem",
+		SvidBundleFileName: "svid_bundle.pem",
+		SvidKeyFileName: "svid.key",
+	}
+	sidecar, err := app.NewSidecar(&config)
+	if err != nil {
+		fmt.Printf("failed to create new sidecar [%s] \n,Exiting ...",err.Error())
+		return
+	}
+	ctx := app.NewContext()
+	err = app.Start(ctx, sidecar)
+	if err != nil {
+		fmt.Printf("failed to start helper [%s] \n",err.Error())
+	} else {
+		dummyChannel := make(chan struct{})
+		fmt.Printf("waiting indefinitely on empty channel \n")
+		<-dummyChannel
+	}
+	fmt.Printf("Exiting ..")
+}
